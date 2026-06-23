ca82cf70556ad853b9f2ba9190ff1a638d30fbd5
Daniel Augusto <dan.saugusto@proton.me>
2026-06-23 18:55:12 -0300

    UPDATE: Modificando o package dos arquivos .java e  aprendendo conceito sobre herança

--- backend/src/main/java/org/example/J1_fundamentos/Pratica/ContaBancaria/heranca/ContaPoupanca.java
+++ backend/src/main/java/org/example/J1_fundamentos/Pratica/ContaBancaria/heranca/ContaPoupanca.java
@@ -1,4 +1,9 @@
 package org.example.J1_fundamentos.Pratica.ContaBancaria.heranca;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends ContaBancaria {
+    private double taxaRendimentoAnual;
+
+    public ContaPoupanca(String titular, double saldoInicial, double taxaRendimentoAnual) {
+        super(titular, saldoInicial);
+    }
 }
