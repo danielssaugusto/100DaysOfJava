b848a5b4e0aaa0ece302fc7435e385d7b4835dd7
Daniel Augusto <dan.saugusto@proton.me>
2026-06-24 12:27:59 -0300

    ACREATE: criação de métodos para ContaPoupanca e e criação da classe de teste BancoDemo.java

---
+++ backend/src/main/java/org/example/J1_fundamentos/Pratica/ContaBancaria/heranca/BancoDemo.java
@@ -0,0 +1,17 @@
+package org.example.J1_fundamentos.Pratica.ContaBancaria.heranca;
+
+public class BancoDemo {
+    static void main() {
+        ContaCorrente cc = new ContaCorrente("Jhon", 2000.0, 5.0);
+        cc.exibirInformacoes();
+        cc.sacar(100.0);
+        cc.exibirInformacoes();
+
+        System.out.println("===================");
+        ContaPoupanca cp = new ContaPoupanca("Mary", 5000, 2.5);
+        cp.exibirInformacoes();
+        cp.aplicarRendimento();
+        cp.exibirInformacoes();
+
+    }
+}
