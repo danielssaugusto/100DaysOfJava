0519c9e3a514d16871f3a0ef0849adde8c55d87a
Daniel Augusto <dan.saugusto@proton.me>
2026-06-28 21:33:11 -0300

    UPDATE: instânciando OperacaoBancaria na classe BancoDemo, atualizaando a chamada dos métodos

--- backend/src/main/java/org/example/J1_fundamentos/Pratica/ContaBancaria/heranca/BancoDemo.java
+++ backend/src/main/java/org/example/J1_fundamentos/Pratica/ContaBancaria/heranca/BancoDemo.java
@@ -3,7 +3,11 @@ package org.example.J1_fundamentos.Pratica.ContaBancaria.heranca;
 public class BancoDemo {
     static void main() {
         // DemoBancario1();
+        // DemoBancario2();
+        OperacaoBancaria op = new OperacaoBancaria(OperacaoBancaria.OP_SAQUE, 500.0);
+    }
 
+    private static void DemoBancario2() {
         ContaCorrente cc = new ContaCorrente("Jhon", 2000.0, 5.0);
         ContaPoupanca cp = new ContaPoupanca("Mary", 5000, 2.5);
 
