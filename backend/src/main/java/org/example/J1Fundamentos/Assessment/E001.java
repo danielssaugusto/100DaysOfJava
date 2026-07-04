1f9d9486d8c8efad8281e67dbce10addc11104c8
Daniel Augusto <dan.saugusto@proton.me>
2026-07-04 00:10:18 -0300

    CREATE: criação da pasta para o Assessment e resolução da primeira questão

---
+++ backend/src/main/java/org/example/J1Fundamentos/Assessment/E001.java
@@ -0,0 +1,10 @@
+package org.example.J1Fundamentos.Assessment;
+
+public class E001 {
+    static void main() {
+        String myName = "Jhon Doe";
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello, my name's " + myName + " and I'm learning Java!");
+    }
+}
