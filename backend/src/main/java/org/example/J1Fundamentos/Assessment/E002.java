82d6e1537ba74eaf1b0b01d8e87d0a85245485e1
Daniel Augusto <dan.saugusto@proton.me>
2026-07-05 19:05:34 -0300

    CREATE: iplementando um validado de senhas

---
+++ backend/src/main/java/org/example/J1Fundamentos/Assessment/E002.java
@@ -0,0 +1,23 @@
+package org.example.J1Fundamentos.Assessment;
+
+public class E002 {
+    String name;
+    String password;
+
+
+    public static boolean passwordValidator(String password) {
+        if (password.length() < 8) {
+            System.out.println("Senha fraca! Sua senha não pode ter menos de oito caracteres.");
+            return false;
+        }
+    }
+}
+
+
+/*
+* Solicite ao usuário seu nome e uma senha.
+A senha deve:
+Ter no mínimo 8 caracteres.
+Conter pelo menos uma letra maiúscula, um número e um caractere especial (@, #, $, etc.).
+Caso a senha seja inválida, o programa deve informar o erro específico e solicitar uma nova tentativa.
+* */
