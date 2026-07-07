2477c13e2cf8ead5e6b01c6c5f61b6c79823bd06
Daniel Augusto <dan.saugusto@proton.me>
2026-07-07 19:54:35 -0300

    UPDATE: usos do try-catch e algumas exceptions em Java

--- backend/src/main/java/org/example/J3_Advance/Exceptions/J002_TryCatch.java
+++ backend/src/main/java/org/example/J3_Advance/Exceptions/J002_TryCatch.java
@@ -1,4 +1,30 @@
 package org.example.J3_Advance.Exceptions;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class J002_TryCatch {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Início do programa.");
+        int result = 10 / 2;
+        System.out.println("Meio do programa - resultado: " + result);
+
+        try {
+            System.out.println("Digite um número: ");
+            int num = sc.nextInt();
+            System.out.println("Você digitou o número: " + num);
+        } catch (InputMismatchException ex) {
+            System.out.println("Error: invalid number.");
+        } catch (NoSuchElementException ex) {
+            System.out.println("Não há elemento para mostrar.");
+        } catch (Exception ex) {
+            System.out.println("Erro genérico.");
+        } finally {
+            System.out.println("Conexão encerrada.");
+        }
+
+    }
 }
