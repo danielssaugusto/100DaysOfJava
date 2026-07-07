6abeed089b1ae0cd0243dac724cac0c505cbc05f
Daniel Augusto <dan.saugusto@proton.me>
2026-07-07 14:23:39 -0300

    UPDATE: uso de ArrayList

---
+++ backend/src/main/java/org/example/Pratica/arrays/UseArrayList.java
@@ -0,0 +1,9 @@
+package org.example.Pratica.arrays;
+
+import java.util.ArrayList;
+
+public class UseArrayList {
+    static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+    }
+}
